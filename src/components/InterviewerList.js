49b8d85daa2c382747a8ec55ec4d38dd83bb38d4
jabrilg <jabrilgadhane@hotmail.ca>
2022-03-01 15:30:26 -0500

    add interview list

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,33 @@
+
+import React from 'react'
+import InterviewerListItem from 'components/InterviewerListItem.js'
+import "./InterviewerList.scss"
+import PropTypes from 'prop-types';
+
+
+export default function InterviewerList(props) {
+  InterviewerList.propTypes = {
+    value: PropTypes.number,
+    onChange: PropTypes.func.isRequired
+  };
+ const interviewers = props.interviewers.map(interviewer => {
+   return(
+      
+      <InterviewerListItem 
+       id={interviewer.id} 
+       name= {interviewer.name}
+       selected={interviewer.id === props.value}
+       avatar = {interviewer.avatar}
+       setInterviewer={(ev)=>props.onChange(interviewer.id)}
+         />
+      
+      )
+     })
+     return( <section className="interviewers">
+
+     <h4 className="interviewers__header text--light">Interviewer
+     </h4>
+   <ul className="interviewers__list">{interviewers}</ul>
+   </section>)
+      };
+      
--- stories/index.js
+++ stories/index.js
@@ -9,8 +9,15 @@ import Button from "components/Button";
 
 import DayListItem from "components/DayListItem"
 
-
 import DayList from "components/DayList"
+
+import InterviewerListItem from "components/InterviewerListItem.js";
+
+import InterviewerList from "components/InterviewerList.js"; 
+
+import Appointment from "components/Appointment/index.js";
+
+import Header from "components/Appointment/Header"
 
 //Button/ is a button that displays a list of buttons 
 storiesOf("Button", module)
@@ -80,3 +87,49 @@ storiesOf("Button", module)
       <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
   ));
 
+
+
+  // interview
+
+  const interviewer = {
+    id: 1,
+    name: "Sylvia Palmer",
+    avatar: "https://i.imgur.com/LpaY82x.png"
+  };
+  
+  storiesOf("InterviewerListItem", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+    })
+    .add("Unselected", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+      />
+    ))
+    .add("Selected", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected
+      />
+    ))
+    .add("Clickable", () => (
+      <InterviewerListItem
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        setInterviewer={action("setInterviewer")}
+      />
+    ));
+
+
+    // appointment
+
+    storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
