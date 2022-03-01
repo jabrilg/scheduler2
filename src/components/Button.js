0d96ad6fdb8b403920532cd151c40caf41b6190b
jabrilg <jabrilgadhane@hotmail.ca>
2022-03-01 13:39:00 -0500

    refactor button

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,3 +5,18 @@ import "components/Button.scss";
 export default function Button(props) {
    return <></>;
 }
+
+const buttonClass = classNames("button", {
+   "button--confirm": props.confirm,
+   "button--danger": props.danger
+ });
+
+ return (
+   <button
+     className={buttonClass}
+     onClick={props.onClick}
+     disabled={props.disabled}
+   >
+     {props.children}
+   </button>
+ );
